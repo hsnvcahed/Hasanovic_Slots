37ce71717c9f06fc9c279bbd89fec4347a979054
hsnvcahed <hasanovic.a02@htlwienwest.at>
2021-10-14 18:02:35 +0200

    Aufgabe 3 fertiggestellt

--- client/src/components/DataTable.vue
+++ client/src/components/DataTable.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="data-table d-flex justify-center flex-wrap">
     <div class="d-flex justify-center"><slot name="header" /></div>
     <table class="d-flex flex-column justify-center">
       <thead>
-        <tr class="green white--text">
+        <tr>
           <th style="width:200px" v-for="(el, id) in headers" :key="id">
             {{ el }}
           </th>
@@ -15,12 +15,13 @@
             {{ itemprop }}
           </td>
         </tr>
-        <tr>
+        <hr />
+        <tr class="font-weight-bold">
           <slot name="bodyappend" />
         </tr>
       </tbody>
     </table>
-    <div class="d-flx justify-center"><slot name="footer" /></div>
+    <div class="d-flex justify-center"><slot name="footer" /></div>
   </div>
 </template>
 
@@ -37,4 +38,40 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped>
+.data-table {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 40px;
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.data-table td,
+th {
+  text-align: left;
+}
+.data-table footer {
+  margin: 30px 0 30px 0;
+  font-style: italic;
+}
+.data-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.data-table th,
+.data-table td {
+  padding: 12px 15px;
+}
+.data-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.data-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,46 +1,49 @@
 <template>
   <div>
-    <DataTable :items="desserts" :headers="headers">
+    <DataTable :items="desserts" :headers="headers" v-if="false">
       <template v-slot:header>
         <h2>Watch What You Eat!</h2>
       </template>
       <template v-slot:footer> Created 2021 by Robert Baumgartner </template>
       <template v-slot:bodyappend>
         <tr class="last-row">
-          <td>Total</td>
-          <td>{{ totalCalories }}</td>
-          <td>{{ totalFat }}</td>
-          <td>{{ totalCarbs }}</td>
-          <td>{{ totalProtein }}</td>
-          <td></td>
+          <td style="width:200px">Total</td>
+          <td style="width:200px">{{ totalCalories }}</td>
+          <td style="width:200px">{{ totalFat }}</td>
+          <td style="width:200px">{{ totalCarbs }}</td>
+          <td style="width:200px">{{ totalProtein }}</td>
+          <td style="width:200px"></td>
         </tr>
       </template>
     </DataTable>
-    <AppLayout v-if="false">
+    <AppLayout v-if="true">
+      <template v-slot:default>
+        <AppButton><img src="home.svg" /> Home</AppButton>
+      </template>
+      <template v-slot:footer>
+        By Robert Baumgartner |
+        <span class="red ma-3 pa-3">
+          Antwort: Nein, die Reihenfolge in der die Slots angegeben werden macht keinen Unterschied
+          Baumgartner
+        </span>
+      </template>
       <template v-slot:header>
         I was made with Vue slots!
       </template>
-      <template v-slot:default>
-        <AppButton><img src="home.svg" /> Home</AppButton>
-
-        <div style="display:flex" v-if="false">
-          <Sheet style="margin-left:20px">
-            <h3>Memo to myself</h3>
-            <p>Always prepare for PLFs in time!</p>
-            <img src="info.svg" width="80px" />
-          </Sheet>
-          <Sheet style="margin-left:20px">
-            <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
-            <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
-            <p>Wish i was there!</p>
-            <span style="font-style: italic">E. Wahn</span>
-          </Sheet>
-        </div>
-      </template>
-      <template v-slot:footer>
-        By Robert Baumgartner
-      </template>
     </AppLayout>
+    <div style="display:flex" v-if="false">
+      <Sheet style="margin-left:20px">
+        <h3>Memo to myself</h3>
+        <p>Always prepare for PLFs in time!</p>
+        <img src="info.svg" width="80px" />
+      </Sheet>
+      <Sheet style="margin-left:20px">
+        <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
+        <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
+        <p>Wish i was there!</p>
+        <span style="font-style: italic">E. Wahn</span>
+      </Sheet>
+    </div>
   </div>
 </template>
 
@@ -56,6 +59,8 @@ export default {
       desserts: [
         ['Cake', 40, 50, 30, 10, 10],
         ['Another Cake', 70, 20, 60, 60, 6],
+        ['More Cake', 70, 20, 60, 60, 6],
+        ['Why is there so much Cake', 70, 20, 60, 60, 6],
       ],
       headers: ['Dessert', 'Calories', 'Fat', 'Carbs', 'Protein', 'Iron'],
     };
@@ -65,6 +70,28 @@ export default {
     AppLayout,
     AppButton,
     DataTable,
+  },
+  computed: {
+    totalCalories() {
+      let res = 0;
+      this.desserts.forEach((d) => (res += d[1]));
+      return res;
+    },
+    totalFat() {
+      let res = 0;
+      this.desserts.forEach((d) => (res += d[2]));
+      return res;
+    },
+    totalCarbs() {
+      let res = 0;
+      this.desserts.forEach((d) => (res += d[3]));
+      return res;
+    },
+    totalProtein() {
+      let res = 0;
+      this.desserts.forEach((d) => (res += d[4]));
+      return res;
+    },
   },
 };
 </script>
